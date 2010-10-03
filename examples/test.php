3a6942a90b169fb3b1dbcd2f648917553f494fc1
Bill Shupp <hostmaster@shupp.org>
2010-10-02 20:25:39 -0700

    Added some fixes and error checking to example

--- examples/test.php
+++ examples/test.php
@@ -4,14 +4,18 @@
 
 set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 
-require_once 'Services/Yadis.php';
+require_once 'HTTP/Request2.php';
+
+require_once 'Zend/Yadis.php';
 foreach (array('http://www.yahoo.com', '=self*shupp') as $id) {
-    $yadis       = new Services_Yadis($id);
+    $yadis       = new Zend_Yadis($id);
     $serviceList = $yadis->discover();
     foreach ($serviceList as $service) {
         $types = $service->getTypes();
-        echo $types[0], ' at ', implode(', ', $service->getUris()), PHP_EOL;
-        echo 'Priority is ', $service->getPriority(), PHP_EOL;
+        if ($service->getUris()) {
+            echo $types[0], ' at ', implode(', ', $service->getUris()), PHP_EOL;
+            echo 'Priority is ', $service->getPriority(), PHP_EOL;
+        }
     }
 }
 
